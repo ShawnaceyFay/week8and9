acc2fca1a849e8f8fc73385e278a091ee00bdbd1
Shawnacey Williams <swilli25@confederationcollege.ca>
2018-10-09 01:41:54 -0400

    Commit initial build, Commit these Changes. Created a message box button, saying "Some Text" It was done because it is part of the cirriculum and I need to pass! Added a text box name txtData Added a text box named txtDuplicate Added a text box named txtOriginal

---
+++ source/repos/My Projects/Week2LabCP370/Week2LabCP370/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Week2LabCP370
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDuplicate = new System.Windows.Forms.TextBox();
+            this.txtOriginal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDuplicate
+            // 
+            this.txtDuplicate.Location = new System.Drawing.Point(78, 77);
+            this.txtDuplicate.Name = "txtDuplicate";
+            this.txtDuplicate.Size = new System.Drawing.Size(100, 20);
+            this.txtDuplicate.TabIndex = 1;
+            // 
+            // txtOriginal
+            // 
+            this.txtOriginal.Location = new System.Drawing.Point(78, 33);
+            this.txtOriginal.Name = "txtOriginal";
+            this.txtOriginal.Size = new System.Drawing.Size(100, 20);
+            this.txtOriginal.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtOriginal);
+            this.Controls.Add(this.txtDuplicate);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDuplicate;
+        private System.Windows.Forms.TextBox txtOriginal;
+    }
+}
+
